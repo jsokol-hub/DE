4af2a36d0375f23e700cd0f4fb1d8cc3aef6b690
Ivan Sokol <ivn.2skl@gmail.com>
2026-03-23 19:19:23 +0200

    Enhance logging in transform function to track input rows, null values, and deduplication process

--- src/transform/clean_cafe_rows.py
+++ src/transform/clean_cafe_rows.py
@@ -3,6 +3,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 def transform(rows):
+    logger.info("Input rows: %s", len(rows))
+    counter_null = 0
     cleaned = []
     for row in rows:
         cleaned_row = {}
@@ -10,6 +12,8 @@ def transform(rows):
             value = value.strip()
             if value == "":
                 value = None
+            if key == "price" and value is None:
+                counter_null +=1
             if (key == "drink" or key == "payment_method" or key == "branch") and value is not None:
                 value = value.lower()
             if key == "order_date" and value is not None and "/" in value:
@@ -17,16 +21,20 @@ def transform(rows):
                 value = f"{value[2]}-{value[1]}-{value[0]}"
             cleaned_row[key] = value
         cleaned.append(cleaned_row)
+    logger.info("NULL value rows: %s", counter_null)
 
+    counter_dupl = 0
     seen = set()
     deduplicated = []
     for row in cleaned:
         key = (row["order_id"],)
         if key in seen:
+            counter_dupl +=1
             continue
         else:
             seen.add(key)
             deduplicated.append(row)
+    logger.info("Deduplicated rows: %s", counter_dupl)
+    logger.info("Output rows: %s", len(deduplicated))
     
     return deduplicated
-    
