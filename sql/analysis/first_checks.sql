86976a6979da8939ee2994040c363203edaf4505
Ivan Sokol <ivn.2skl@gmail.com>
2026-03-24 16:19:07 +0200

    Remove SSH tunnel settings from .env.example, delete pytest.ini configuration file, and update SQL queries in first_checks.sql to remove schema references for cleaner access.

--- sql/analysis/first_checks.sql
+++ sql/analysis/first_checks.sql
@@ -1,30 +1,30 @@
 SELECT COUNT(order_id) as "Total ordred"
-FROM de_project.staging.cafe_sales;
+FROM staging.cafe_sales;
 
 SELECT branch, COUNT(order_id) as "Total ordred per branch"
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 GROUP BY branch;
 
 SELECT drink, COUNT(drink) as "Drink count"
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 GROUP BY drink
 ORDER BY COUNT(drink) DESC
 LIMIT 1;
 
 SELECT AVG(CAST(price AS NUMERIC)) AS "Avg price", branch
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 GROUP BY branch;
 
 SELECT order_id, customer_name
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 WHERE customer_name IS NULL;
 
 SELECT order_date, SUM(CAST(price AS NUMERIC)) AS "Income"
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 GROUP BY order_date;
 
 SELECT customer_name, COUNT(order_id)
-FROM de_project.staging.cafe_sales
+FROM staging.cafe_sales
 GROUP BY customer_name
 ORDER BY COUNT(order_id) DESC
 LIMIT 3;
